66cbf541d630755953e9a14f8d82f898245ede8a
mic-meier <mic.l.meier@gmail.com>
2019-12-14 09:47:45 +0100

    unicafe - step2 + step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Statistics = ({ good, bad, all }) => {
+  const average = (good - bad) / 9;
+  const positive = (good / all) * 100;
+  return (
+    <div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
+    </div>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
 
   return (
     <div>
@@ -16,6 +28,8 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <Statistics good={good} bad={bad} all={all} />
     </div>
   );
 };
