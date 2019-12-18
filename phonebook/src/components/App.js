74e30a3a00102f06753a4a9573958eeaeceae93a
mic-meier <mic.l.meier@gmail.com>
2019-12-18 19:01:05 +0100

    phonebook - step4

--- phonebook/src/components/App.js
+++ phonebook/src/components/App.js
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phoneNumber: "666-66-66" }
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" }
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const contacts = () =>
-    persons.map(person => (
+  const contacts = () => {
+    const displayContacts = persons.filter(person =>
+      person.name.toUpperCase().includes(searchTerm.toUpperCase())
+    );
+    return displayContacts.map(person => (
       <div key={person.name}>
-        {person.name} {person.phoneNumber}
+        {person.name} {person.number}
       </div>
     ));
+  };
 
   const addContact = event => {
     event.preventDefault();
@@ -20,9 +28,9 @@ const App = () => {
     if (persons.map(person => person.name).includes(newName)) {
       alert(`${newName} is already added to the phone book`);
     } else {
-      setPersons(persons.concat({ name: newName, phoneNumber: newNumber }));
+      setPersons(persons.concat({ name: newName, number: newNumber }));
       setNewName("");
-      setNewNumber("")
+      setNewNumber("");
     }
   };
 
@@ -32,11 +40,19 @@ const App = () => {
 
   const handleNumberChange = e => {
     setNewNumber(e.target.value);
-  }
+  };
+
+  const handleSearch = e => {
+    setSearchTerm(e.target.value);
+  };
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Phone Book</h2>
+      <div>
+        Filter: <input value={searchTerm} onChange={handleSearch} />
+      </div>
+      <h2>Add a new contact</h2>
       <form onSubmit={addContact}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
