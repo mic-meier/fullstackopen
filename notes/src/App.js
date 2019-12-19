b01ed7abd5f1a5c6a68e85330508e4580f59496b
mic-meier <mic.l.meier@gmail.com>
2019-12-19 18:29:23 +0100

    phonebook - step6

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
 
-function App() {
+const App = props => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then(res => {
+      console.log("promise fulfilled");
+      setNotes(res.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes")
+
+  const rows = () => notes.map(note => <Note key={note.id} note={note} />);
+
+  const handleNoteChange = event => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const addNote = event => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Notes</h1>
+      <ul>{rows()}</ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
