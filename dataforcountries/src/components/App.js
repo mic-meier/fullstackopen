66e6fcf953b3093416215b04baa08ccc3a068198
mic-meier <mic.l.meier@gmail.com>
2019-12-24 11:39:22 +0100

    dataforcountries - step3

--- dataforcountries/src/components/App.js
+++ dataforcountries/src/components/App.js
@@ -7,6 +7,7 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [detailedCountry, setDetailedCountry] = useState(null);
+  const [weather, setWeather] = useState(null);
 
   useEffect(() => {
     axios
@@ -14,10 +15,20 @@ const App = () => {
       .then(res => setCountries(res.data));
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${
+          process.env.REACT_APP_API_KEY
+        }&query=${detailedCountry ? detailedCountry.capital : null}`
+      )
+      .then(res => setWeather(res.data));
+  }, [detailedCountry]);
+
   const handleSearch = e => {
     const filtered = countries.filter(country =>
       country.name.toUpperCase().includes(e.target.value.toLocaleUpperCase())
-    )
+    );
     if (filtered.length === 1) {
       setDetailedCountry(filtered[0]);
     } else {
@@ -28,7 +39,7 @@ const App = () => {
 
   const handleClick = (e, country) => {
     console.log("country", country);
-    setDetailedCountry(country)
+    setDetailedCountry(country);
   };
 
   const filteredCountries = countries.filter(country =>
@@ -39,7 +50,7 @@ const App = () => {
     <div>
       Search countries: <input value={searchTerm} onChange={handleSearch} />
       <div>
-        <CountryDetails country={detailedCountry} />
+        <CountryDetails country={detailedCountry} weather={weather} />
         <DisplayCountries
           filteredCountries={filteredCountries}
           handleClick={handleClick}
--- dataforcountries/src/components/CountryDetails.js
+++ dataforcountries/src/components/CountryDetails.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-const countryDetails = ({ country }) => {
+const countryDetails = ({ country, weather }) => {
+  let currentWeather;
+
   if (!country) {
-    return null
+    return null;
   }
+
+  if (weather) {
+    currentWeather = weather.current;
+  }
+
+  console.log("currentWeather", currentWeather);
 
   const languages = country.languages.map(language => (
     <li key={language.name}>{language.name}</li>
@@ -17,6 +25,16 @@ const countryDetails = ({ country }) => {
       <h2>Languages</h2>
       <ul>{languages}</ul>
       <img src={country.flag} alt={`Flag of ${country.name}`} width="300" />
+      <h2>Weather in {country.capital}</h2>
+      <div>
+        <strong>Temperature: </strong>
+        {currentWeather.temperature} Celsius
+      </div>
+      <img src={currentWeather.weather_icons[0]} alt="" />
+      <div>
+        <strong>Wind: </strong>
+        {currentWeather.wind_speed} kph direction {currentWeather.wind_dir}
+      </div>
     </div>
   );
 };
